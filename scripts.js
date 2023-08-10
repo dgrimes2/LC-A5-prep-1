b430e5f538b7489ee2a181e9dc2b4cc71a54e679
dgrimes2 <grime22d@mtholyoke.edu>
2023-08-09 19:53:24 -0500

    added DOM functionality

--- scripts.js
+++ scripts.js
@@ -26,17 +26,76 @@ let palettes = [
 // HT: Hex values for palettes copied, rather efficiently, from https://coolors.co/palettes/trending
 
 // TODO: Establish variable to track current palette
-
+let currentPalette = {"name": "", "colors": [] };
 
 // TODO: Add listener to make sure page elements have completely loaded
-
+window.addEventListener("load", function() {
+  console.log("Page loaded.");
+  // If we were fetching JSON from elsewhere, we'd do it here and call init() from inside
+  console.log("Data loaded.");
+  // For now we'll just tell the browser that the DOM code can run!
+  init();
+});
 
 // Function handles all basic functionality once page has loaded
 function init() {
 
   // TODO: Code reference objects needed for dynamic functionality
-
+  let header = document.getElementById("header");
+  let coloredText = document.getElementById("colored-text");
+  let button = document.getElementById("get-colors");
+  let paletteName = document.getElementById("palette-name");
+  // to store all five div elements (HTML)
+  let colorBlocks = [];  
+  // to store all five p elements (HTML)
+  let colorCodes = [];
+  for (let i = 0; i < 5; i++) {
+    colorBlocks.push(document.getElementById("color" + i));
+    colorCodes.push(document.getElementById("code" + i));
+  };
 
   // TODO: Code event listeners and functions
+  function changePalette() {
+    let randomIndex;
+    
+    // pop-up alert: only if it's the first time clicking the button
+    if(currentPalette.name === "") {
+      window.alert("Let's get rid of this boring, monochromatic look and add some pizzazz!!");
+    };
+    // add validator
+
+    do {
+      randomIndex = Math.floor(Math.random() * palettes.length);
+      currentPalette = palettes[randomIndex];
+    } while(paletteName.innerHTML === currentPalette.name)
+    // to have this show up in dev tools
+    console.log(`Color palette is now ${currentPalette.name}.`);
+    
+    // console.log(currentPalette);
+
+    // to have the current color name show up above the blocks
+    paletteName.innerHTML = currentPalette.name;
+    // so that the colors actually show up in the blocks
+    for (let i = 0; i < 5; i++) {
+      colorBlocks[i].style.backgroundColor = currentPalette.colors[i];
+      // to show the color's codes under each block AND uppercase
+      colorCodes[i].innerHTML = currentPalette.colors[i].toUpperCase();
+    }
+
+    // change the header background color to first color in current Palette (color0)
+    header.style.backgroundColor = currentPalette.colors[0];
+    // change the random generator text color to LAST color in current Palette (color4)
+    coloredText.style.color = currentPalette.colors[4];
+  }
+  // listener that corresponds to button clicking
+  button.addEventListener("click", changePalette);
+  // listener that corresponds to color changing when button hovering
+  button.addEventListener("mouseover", function() {
+    button.style.backgroundColor = currentPalette.colors[4];
+  });
+  // listener that corresponds to color changing when button hovering is done
+  button.addEventListener("mouseout", function() {
+    button.style.backgroundColor = "#222";
+  });
 }
 
